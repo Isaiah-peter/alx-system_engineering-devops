6f3a7cc63f6b37002b88e4b8f2ea9725431fee17
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-07 22:54:23 +0100

    task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.'''
+"""Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her todo list progress."""
 
 
 import requests
